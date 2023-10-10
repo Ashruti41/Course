880b7f5d8adf4bd8b1c65e1df22a61912a3ce9f9
Ashruti41 <98145165+Ashruti41@users.noreply.github.com>
2023-10-10 12:33:33 +0530

    Create fav_number.c

---
+++ 10-10-2023/fav_number.c
@@ -0,0 +1,18 @@
+// enter your fav number and print that number
+
+#include <stdio.h>
+
+int main()
+{
+    int myFavNO = 0;
+
+    printf("Enter your favourite number\n");
+    scanf("%d", &myFavNO);
+    printf("Your Favourite Number is: %d\n", myFavNO);
+}
+
+/* output
+  Enter your favourite number
+  18
+  Your Favourite Number is: 18
+  */
